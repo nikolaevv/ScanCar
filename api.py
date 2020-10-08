2dd28ed1cb4ca3074fe0e9e909804a28e4a9b7ca
nikolayevv <starlei165@gmail.com>
2020-10-08 23:35:57 +0300

    Создал метод распознавания по одному фото с использованием API втб

--- api.py
+++ api.py
@@ -3,10 +3,45 @@
 from flask import Flask, render_template, send_from_directory, session, redirect, url_for, escape, request
 from flask_sqlalchemy import SQLAlchemy
 from app import models, app, db
-#from werkzeug.utils import secure_filename
+from flask_api import status
+from werkzeug.utils import secure_filename
 import os
+from config import token
+import base64
+import requests
+import time
 
-@app.route('/', methods = ['POST'])
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/api/photo/recognize', methods = ['POST'])
 def scan_photo():
-    if request.method == 'POST':
-        photo = request.form['new_exercise']
+    # Распознавание одного фото
+
+    photo = request.files['photo']
+    print(photo)
+
+    if photo and allowed_file(photo.filename):
+        #with open("my_image.jpg", "rb") as img_file:
+        my_string = base64.b64encode(photo.read())
+        # Преобразование в base-64
+        headers = {'X-IBM-Client-Id': token, 'content-type': "application/json", 'accept': "application/json"}
+    
+        try:
+            response = requests.post('https://gw.hackathon.vtb.ru/vtb/hackathon/car-recognize', headers = headers, json = {'content': str(my_string, 'utf-8')}).json()
+            # Запрос к VTB-API
+            
+            probabilities = response['probabilities']
+            favourite = max(probabilities, key = probabilities.get)
+            # Фаворит нейросети ВТБ
+        except Exception:
+            return {'error': 'Photo was not recognized'}, status.HTTP_400_BAD_REQUEST
+
+        return favourite
+    return {'error': 'Invalid type of photo'}, status.HTTP_400_BAD_REQUEST
+        
+
+app.secret_key = os.urandom(24)
+app.run(debug=True)
--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+UPLOAD_FOLDER = 'photos'
+token = '00978ffb1ced950ecf1af6dc69351fa1'
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
